adc17245b0481fb9147bcb2b0eb96cb3b0abaa0c
Johan Hanses <johanhanses@gmail.com>
2020-09-03 20:35:06 +0200

    got a whole lotta tailwind

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -103,5 +103,145 @@
             <div class="text-gray-400 mt-1">Playstation 4</div>
         </div>
     </div>
+    <div class="flex my-10">
+        <div class="recently-reviewed w-3/4 mr-32">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
+            <div class="recently-reviewed-container space-y-12 mt-8">
+                <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px;bottom: -20px;">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                        </div>
+                    </div>
+                    <div class="ml-12">
+                        <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                            Final Fantasy 7 Remake
+                        </a>
+                        <div class="text-gray-400 mt-1">PS4</div>
+                        <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis accusantium explicabo consequatur inventore voluptate eum odio id adipisci. Ut necessitatibus molestiae, deleniti corrupti aliquam eos quod vitae voluptatem inventore labore!</p>
+                    </div>
+                </div>
+                <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px;bottom: -20px;">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                        </div>
+                    </div>
+                    <div class="ml-12">
+                        <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                            Final Fantasy 7 Remake
+                        </a>
+                        <div class="text-gray-400 mt-1">PS4</div>
+                        <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis accusantium explicabo consequatur inventore voluptate eum odio id adipisci. Ut necessitatibus molestiae, deleniti corrupti aliquam eos quod vitae voluptatem inventore labore!</p>
+                    </div>
+                </div>
+                <div class="game bg-gray-800 rounded-lg shadow-md flex p-6">
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img src="/ff7.jpg" alt="game cover" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right: -20px;bottom: -20px;">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                        </div>
+                    </div>
+                    <div class="ml-12">
+                        <a href="#" class="block text-lg font-semibold leading-tight hover:text-gray-400 mt-4">
+                            Final Fantasy 7 Remake
+                        </a>
+                        <div class="text-gray-400 mt-1">PS4</div>
+                        <p class="mt-6 text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis accusantium explicabo consequatur inventore voluptate eum odio id adipisci. Ut necessitatibus molestiae, deleniti corrupti aliquam eos quod vitae voluptatem inventore labore!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="most-anticipated w-1/4">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Most Anticipated</h2>
+            <div class="most-anticipated-container space-y-10 mt-8">
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+            </div>
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold mt-10">Coming Soon</h2>
+            <div class="most-anticipated-container space-y-10 mt-8">
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+                <div class="game flex">
+                    <a href="#">
+                        <img src="/ff7.jpg" alt="game cover" class=" w-16 hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="ml-4">
+                        <a href="#" class="hover:text-gray-300">Final Fantasy 7 Remake</a>
+                        <p class="text-gray-400 text-sm mt-1">Sept 16, 2020</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
 @endsection
